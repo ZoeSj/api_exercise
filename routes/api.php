760762e6e9329b66257672ad80b03516de334dd6
shengjia <shengj@anchnet.com>
2018-07-16 13:59:00 +0800

    api

---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api'
+], function ($api) {
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function ($api) {
+        $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
+
+        //短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+
+        //用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+
+        //图片验证码
+        $api->post('captachas','CaptachasController@store')
+            ->name('api.captachas.store');
+    });
+
+});
+
